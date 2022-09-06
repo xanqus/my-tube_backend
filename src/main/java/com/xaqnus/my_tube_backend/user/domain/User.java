9efb027069928c726d3f93e984406c49323ef0b8
xanqus <sbk98031816@gmail.com>
2022-09-06 09:39:34 +0900

    add unique and nullable false at user table column username and email

--- src/main/java/com/xaqnus/my_tube_backend/user/domain/User.java
+++ src/main/java/com/xaqnus/my_tube_backend/user/domain/User.java
@@ -16,8 +16,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(unique=true, nullable = false)
     private String username;
 
+    @Column(unique=true, nullable = false)
     private String email;
 
     private String password;
